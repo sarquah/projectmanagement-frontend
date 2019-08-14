b2e91b8c311226cf66388d97e9c7716b08faeb9b
Stephen Sarquah <stephen.sarquah@gmail.com>
2019-08-14 17:26:42 +0200

    Changed config to use typescript and recognize imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
+import Projects from './Projects';
 
 const App: React.FC = () => {
   useEffect(() => {
@@ -13,6 +14,7 @@ const App: React.FC = () => {
   return (
     <div>
       <h1>Project Management</h1>
+      <Projects />
     </div>
   );
 };
